4e27a7fbc9214cc6e8922b039ec6cd2cbdd1debb
linkem82 <emilio.sellitto@icloud.com>
2014-05-11 20:21:35 +0200

    commit2

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,12 @@
-var server = io.connect("htpp://localhost:8080");
+$(document).ready(function() {
+	var server = io.connect("htpp://localhost:8080");
 
-$('#send_message').submit(function(e){
-	var message = $('#message').val();
-	socket.emit('messages', message);
+
+	$('#send_button').submit(function(event){		
+		alert("Ciao");
+		event.preventDefault();
+		var message = $('#message').val();
+		socket.emit('messages', message);
+	});
+	var button = $('#send_button');
 });
