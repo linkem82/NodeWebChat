df70f0ebdadcdbcc492e6c8dfc2a02324586f4eb
Emilio <emilio.sellitto@gmail.com>
2014-05-14 17:10:47 +0200

    commi6

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var http = require('http');
 var socket = require('socket.io');
 var path = require('path');
+var redis = require('redis'); 
+var os=require('os');
 
 var app = express();
 var server = http.createServer(app);
 var io = socket.listen(server);
 server.listen(8080);
 
-var os=require('os');
 var ifaces=os.networkInterfaces();
 var server_address;
 
@@ -24,11 +25,34 @@ console.log(server_address);
 
 app.use(express.static(path.resolve('../frontend')));
 
+var redisClient = redis.createClient();
+ 
+var addMessage = function(name, data) {
+	var message = JSON.stringify({name: name, data: data});
+	redisClient.lpush("messages", message, function(err, response) {
+		redisClient.ltrim("messages", 0, 10);
+	});
+}
+
 io.sockets.on("connection", function(client) {
-	console.log("Client connected...");	
-	client.on('messages', function(data) {
-		console.log(data);
-		client.broadcast.emit('message', data);
+		
+	client.on("join", function(name) {		
+		client.set("nickname", name);
+		redisClient.lrange("messages", 0, -1, function(err, messages) {
+			messages = messages.reverse();
+			messges.forEach(function(message) {
+				message = JSON.parse(message);
+				client.emit("messages", message.name + ": " + message.data);
+			});
+		});
+		console.log("Client " + name + " connected...");
+	})
+	client.on('messages', function(message) {
+		console.log(message);
+		client.get("nickname", function(err, nickname) {
+			console.log(nickname + ": " + message);
+			client.broadcast.emit("message", nickname + ": " + message);
+		})		
 	});
 });
 
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,6 @@
-var insertMessage = function(message) {
-	var messageEl = $('<li>' + message + '<li/>');
+var insertMessage = function(message, styleClass) {
+	var messageEl = $('<li>' + message + '</li>');
+	messageEl.addClass(styleClass);
 	$('#messages').append(messageEl);
 }
 
@@ -7,19 +8,25 @@ $(document).ready(function() {
 
 	var server_url = $(location).attr('href');;
 	var socket = io.connect(server_url);
+	var nickname;
 
-	socket.on('message', function(data) {
-		insertMessage(data);
-		$('body').scrollTop($(document).height());
-		//alert($('body').scrollTop());
+	socket.on("connect", function() {
+		$("#status").html("Status: connected");
+		nickname = prompt("Insert your nickname");
+		socket.emit("join", nickname);
 	});
 
+	socket.on('message', function(data) {
+		insertMessage(data, "sender");
+		$('body').scrollTop($(document).height());		
+	});
 
 	$('#send_message').submit(function(event){		
 		event.preventDefault();
 		var message = $('#message').val();
 		socket.emit('messages', message);
+		insertMessage("Io: " + message, "receiver");
 		$('#message').val('');
 	});
-	var button = $('#send_button');
+	
 });
